2529d0c18e7c14aeb33df1ca8f3c4d37e8fb623d
angelalin2004 <adl040@ucsd.edu>
2015-09-05 04:12:55 -0700

    added corner cases for deactivated users and titles. need to fix bug with tags

--- javascript.js
+++ javascript.js
@@ -1,25 +1,53 @@
-$("div.reblog-list").after("<p class='test insert-here'></p>");
-$(".test").width(function(n, c){
-        return c - 40;
-        });
-
+$("div.reblog-list").after("<p class='newList insert-here'></p>");
+$(".newList").width(function(n, c){
+    return c - 40;
+    });
 
 var reblogLists = document.querySelectorAll(".reblog-list");
-var newLists = document.querySelectorAll(".test");
-console.log("Number of lists: " + reblogLists.length);
-console.log("Number of NEW lists: " + newLists.length);
+var newLists = document.querySelectorAll(".newList");
 for (var i = 0; i < reblogLists.length; i++) {
 	var reblogItems = reblogLists[i].getElementsByClassName("reblog-list-item");
-	console.log("Items in list " + i + ": " + reblogItems.length);
 	for ( var j = reblogItems.length - 1; j >= 0; j-- ) {
-		var username = reblogItems[j].firstChild.childNodes[1].textContent;
-		var usernameLink = 
-			reblogItems[j].firstChild.childNodes[1].attributes.getNamedItem("href").value;
-		var comment = reblogItems[j].childNodes[1].innerHTML;
-		console.log(username);
+		// if there is a header, insert it before the new reblog list
+		var reblogTitles = reblogItems[j].getElementsByClassName("reblog-title");
+		if (reblogTitles.length > 0) {
+			for (var k = reblogTitles.length-1; k >= 0; k--) {
+				$(reblogLists[i]).after("<div class='reblog-title reblogTitle'>" + reblogTitles[k].textContent + "</div>");
+			}
+		}
+
+		var userTag = reblogItems[j].firstChild.childNodes[1];
+		var usernameString;
+		// deactivated user case
+		if ($(userTag).hasClass("inactive")) {
+			usernameString = "<span class='deactivated'>" + 
+				userTag.firstChild.nodeValue + "-deactivated</span>:";
+		}
+		else {
+			
+			var username = userTag.textContent;
+			var usernameLink = 
+				userTag.attributes.getNamedItem("href").value;
+			usernameString = "<a href='" + usernameLink + "'>" + username + "</a>:";
+			
+		}
+		var comment = reblogItems[j].getElementsByClassName("reblog-content")[0].innerHTML;
+		var blockquoteMargin = (j==reblogItems.length-1) ? "style='margin-bottom: 0px;'" : "";
 		var insertHeres = reblogLists[i].parentNode.getElementsByClassName("insert-here");
-		insertHeres[insertHeres.length-1].innerHTML = "<p><a href='" + usernameLink + "'>" + 
-			username + ":</a><blockquote class='insert-here'>" + comment + 
+		insertHeres[insertHeres.length-1].innerHTML = "<p>" + usernameString + 
+			"<blockquote class='insert-here' " + blockquoteMargin + ">" + comment + 
 			"</blockquote></p>" + insertHeres[insertHeres.length-1].innerHTML;
 	}
+	// reduce source footer margin
+	/*
+	var sourceFooter = reblogLists[i].parentNode.getElementsByClassName("post-source-footer")[0];
+	$(sourceFooter).css("margin-top","-15px");
+	*/
 }
+
+$(".reblog-list").remove();
+//$(".newList").addClass("reblog-list");
+
+$(".reblogTitle").width(function(n, c){
+	return c - 20;
+});
