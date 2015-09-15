a2b2f98d7c32224387bf9ba37874653cf50146cd
angelalin2004 <adl040@ucsd.edu>
2015-09-14 21:40:48 -0700

    some more fixes, including deactivated username look

--- javascript.js
+++ javascript.js
@@ -18,8 +18,9 @@ for (var i = 0; i < reblogLists.length; i++) {
 			var comment = contributedContent[m].getElementsByClassName("reblog-content")[0].innerHTML;
 			var insertHeres = reblogLists[i].parentNode.getElementsByClassName("insert-here");
 			var blockquoteMargin = "style='margin-bottom: 0px;'";
-			insertHeres[insertHeres.length-1].innerHTML = "<p class='insert-here' " + blockquoteMargin + ">" 
-				+ comment + "</p>" + insertHeres[insertHeres.length-1].innerHTML;
+			insertHeres[insertHeres.length-1].innerHTML = 
+				"<p class='insert-here' " + blockquoteMargin + ">" + comment + 
+				"</p>" + insertHeres[insertHeres.length-1].innerHTML;
 			$(contributedContent[m]).remove();
 		}
 	}
@@ -29,7 +30,8 @@ for (var i = 0; i < reblogLists.length; i++) {
 		var reblogTitles = reblogItems[j].getElementsByClassName("reblog-title");
 		if (reblogTitles.length > 0) {
 			for (var k = reblogTitles.length-1; k >= 0; k--) {
-				$(reblogLists[i]).after("<div class='reblog-title reblogTitle'>" + reblogTitles[k].textContent + "</div>");
+				$(reblogLists[i]).after("<div class='reblog-title reblogTitle'>" 
+					+ reblogTitles[k].textContent + "</div>");
 			}
 			for (var k = reblogTitles.length-1; k >= 0; k--) {
 				$(reblogTitles[k]).remove();
@@ -40,26 +42,30 @@ for (var i = 0; i < reblogLists.length; i++) {
 		var usernameString;
 		// deactivated user case
 		if ($(userTag).hasClass("inactive")) {
-			usernameString = "<span class='deactivated'>" + 
-				userTag.firstChild.nodeValue + "-deactivated</span>:";
+			usernameString = "<span class='deactivated'><u>" + 
+				userTag.firstChild.nodeValue + "</u>:" +
+				// commenting out Deactivated status because display will
+				// not change
+				//"<span class='deactivated-status'>Deactivated</span>" +
+				"</span>";
 		}
 		else {
-			
 			var username = userTag.textContent;
-			var usernameLink = 
-				userTag.attributes.getNamedItem("href").value;
-			usernameString = "<a href='" + usernameLink + "'>" + username + "</a>:";
-			
+			var usernameLink = userTag.attributes.getNamedItem("href").value;
+			usernameString = 
+				"<a href='" + usernameLink + "'>" + username + "</a>:";
 		}
 		var insertHeres = reblogLists[i].parentNode.getElementsByClassName("insert-here");
 		var comment = "";
 		if (reblogItems[j].getElementsByClassName("reblog-content").length > 0) {
 			comment = reblogItems[j].getElementsByClassName("reblog-content")[0].innerHTML;
 			// if there's a comment, add user, blockquote, and comment
-			var blockquoteMargin = (j==reblogItems.length-1 && contributed==false) ? "style='margin-bottom: 0px;'" : "";
-			insertHeres[insertHeres.length-1].innerHTML = "<p>" + usernameString + 
-				"<blockquote class='insert-here' " + blockquoteMargin + ">" + comment + 
-				"</blockquote></p>" + insertHeres[insertHeres.length-1].innerHTML;
+			var blockquoteMargin = 
+				(j==reblogItems.length-1 && contributed==false) ? "style='margin-bottom: 0px;'" : "";
+			insertHeres[insertHeres.length-1].innerHTML = 
+				"<p>" + usernameString + "<blockquote class='insert-here' " + 
+				blockquoteMargin + ">" + comment + "</blockquote></p>" + 
+				insertHeres[insertHeres.length-1].innerHTML;
 		}
 		else {
 			// if there's no comment, don't add anything
@@ -99,15 +105,39 @@ $(".reblogTitle").width(function(n, c){
 var contributedContent = document.querySelectorAll(".contributed-content");
 if (contributedContent.length > 0) {
 	for (var m = 0; m < contributedContent.length; m++) {
-		if ($(contributedContent[m].parentNode).hasClass("post_content_inner")) {
+		if ($(contributedContent[m].parentNode).hasClass("post_content_inner") ||
+			$(contributedContent[m].parentNode).hasClass("post_container")) {
 			var comment = contributedContent[m].getElementsByClassName("reblog-content")[0].innerHTML;
 			var blockquoteMargin = "style='margin-bottom: 0px;'";
-			$(contributedContent[m]).after("<div class='insert-here contributed-no-list' " + blockquoteMargin + ">" 
-				+ comment + "</div>");
+			$(contributedContent[m]).after(
+				"<div class='insert-here contributed-no-list' " + 
+				blockquoteMargin + ">" + comment + "</div>");
 			$(contributedContent[m]).remove();	
 		}
 	}
 }
+
+$(".contributed-no-list").width(function(n, c){
+	return c - 40;
+});
+
+/* doesn't work, may be incompatible with Tumblr's framework
+$(document).ready(function(){
+	$(".deactivated").mouseenter(function(event){
+	  $("#deactivated-status").css("display","inline");
+	});
+
+	$(".deactivated").mouseleave(function(event){
+	  $("#deactivated-status").css("display","none");
+	});
+});
+*/
+
+/* go back to this if the above has any issues
+$(".insert-here.contributed-no-list").width(function(n, c){
+	return c - 20;
+});
+*/
 
 //var endTime = new Date().getTime();
 //console.log("Tumblr Posts Fix finished in " + (endTime-startTime) + "ms");
